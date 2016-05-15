9e395ec268cc2c3f5d00e43b2a8edaa54eb31e65
Sahand Nayebaziz <sahand@sahand.me>
2016-05-15 19:28:04 -0400

    make renderDeep public

--- StateView/StateView.swift
+++ StateView/StateView.swift
@@ -39,7 +39,7 @@ public class StateView: UIView {
         return [:]
     }
     
-    func renderDeep() {
+    public func renderDeep() {
         render()
         shadow.didPlaceAll(resolveProps(self.props))
     }
